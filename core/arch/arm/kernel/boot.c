8f228e9140944c05172806c92fb76a7647237e85
Jorge Ramirez-Ortiz <jorge@foundries.io>
2019-10-27 16:40:45 +0100

    [FIO toup] Support overlay at fix address and extend DTS
    The following commit adds support to extend any DTS passed as an input
    parameter to OPTEE during boot as well as generate a OPTEE specific
    DTS overlay at a build specific address (CFG_OVERLAY_ADDR).
    
    When this feature is enabled, CFG_DT_ADDR and CFG_EXTERNAL_DTB_OVERLAY
    are no longer required.
    
    Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
    Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

--- core/arch/arm/kernel/boot.c
+++ core/arch/arm/kernel/boot.c
@@ -79,6 +79,9 @@ struct dt_descriptor {
 	void *blob;
 #ifdef _CFG_USE_DTB_OVERLAY
 	int frag_id;
+#ifdef CFG_OVERLAY_ADDR
+	int is_overlay;
+#endif
 #endif
 };
 
@@ -661,7 +664,9 @@ static TEE_Result release_external_dt(void)
 
 	return TEE_SUCCESS;
 }
+#ifndef CFG_OVERLAY_ADDR
 boot_final(release_external_dt);
+#endif
 
 #ifdef _CFG_USE_DTB_OVERLAY
 static int add_dt_overlay_fragment(struct dt_descriptor *dt, int ioffs)
@@ -670,6 +675,10 @@ static int add_dt_overlay_fragment(struct dt_descriptor *dt, int ioffs)
 	int offs;
 	int ret;
 
+#ifdef CFG_OVERLAY_ADDR
+	if (!dt->is_overlay)
+		return ioffs;
+#endif
 	snprintf(frag, sizeof(frag), "fragment@%d", dt->frag_id);
 	offs = fdt_add_subnode(dt->blob, ioffs, frag);
 	if (offs < 0)
@@ -1118,6 +1127,9 @@ static void init_external_dt(unsigned long phys_dt)
 
 	dt->blob = fdt;
 
+#ifdef CFG_OVERLAY_ADDR
+	if (dt->is_overlay) {
+#endif
 	ret = init_dt_overlay(dt, CFG_DTB_MAX_SIZE);
 	if (ret < 0) {
 		EMSG("Device Tree Overlay init fail @ %#lx: error %d", phys_dt,
@@ -1125,6 +1137,9 @@ static void init_external_dt(unsigned long phys_dt)
 		panic();
 	}
 
+#ifdef CFG_OVERLAY_ADDR
+	}
+#endif
 	ret = fdt_open_into(fdt, fdt, CFG_DTB_MAX_SIZE);
 	if (ret < 0) {
 		EMSG("Invalid Device Tree at %#lx: error %d", phys_dt, ret);
@@ -1419,6 +1434,10 @@ static void init_primary(unsigned long pageable_part, unsigned long nsec_entry)
  */
 void __weak boot_init_primary_late(unsigned long fdt)
 {
+#ifdef CFG_OVERLAY_ADDR
+	struct dt_descriptor *dt = &external_dt;
+	dt->is_overlay = 0;
+#endif
 	init_external_dt(fdt);
 	tpm_map_log_area(get_external_dt());
 	discover_nsec_memory();
@@ -1447,6 +1466,15 @@ void __weak boot_init_primary_late(unsigned long fdt)
 	} else {
 		init_tee_runtime();
 	}
+
+#ifdef CFG_OVERLAY_ADDR
+	release_external_dt();
+	dt->is_overlay = 1;
+	init_external_dt(CFG_OVERLAY_ADDR);
+	update_external_dt();
+	release_external_dt();
+#endif
+
 	call_finalcalls();
 	IMSG("Primary CPU switching to normal world boot");
 }
